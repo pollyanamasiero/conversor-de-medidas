38ee8a170fe282383486b78e63b0ad2d5834365a
Pollyana Masiero Torquato <113457708+pollyanamasiero@users.noreply.github.com>
2024-05-09 18:53:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function converter() {
+    let num = parseFloat(document.getElementById('num').value)
+    let uni = document.getElementById('uni').value
+    let res = document.getElementById('res')
+
+    let mm = num * 1000
+    let cm = num * 100
+    let dm = num * 10
+    let dam = num / 10
+    let hm = num / 100
+    let km = num / 1000
+
+    switch (uni) {
+        case '1':
+            res.innerHTML = `<p>Resultado: ${num}m = ${mm} milímetros.</p>`
+            break;
+        case '2':
+            res.innerHTML = `<p>Resultado: ${num}m = ${cm} centímetros.</p>`
+            break;
+        case '3':
+            res.innerHTML = `<p>Resultado: ${num}m = ${dm} decímetros.</p>`
+            break;
+        case '4':
+            res.innerHTML = `<p>Resultado: ${num}m = ${dam} decâmetros.</p>`
+            break;
+        case '5':
+            res.innerHTML = `<p>Resultado: ${num}m = ${hm} hectômetros.</p>`
+            break;
+        case '6':
+            res.innerHTML = `<p>Resultado: ${num}m = ${km} quilômetros.</p>`
+            break;
+        default:
+            res.innerHTML = `<p>Insira uma das opções.</p>`
+            break;
+    }
+}
